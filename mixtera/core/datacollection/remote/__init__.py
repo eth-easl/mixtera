1bb39202f56fb8e69a1eae43a5aef7468695c0f7
Xiaozhe Yao <askxzyao@gmail.com>
2024-02-12 16:16:54 +0100

    create automodule for data collection

--- mixtera/core/datacollection/remote/__init__.py
+++ mixtera/core/datacollection/remote/__init__.py
@@ -1,11 +1,8 @@
 """
 This submodule contains Mixtera's remote collection
 """
-
-import os
-
 from .remote_collection import RemoteDataCollection  # noqa: F401
 
-files = os.listdir(os.path.dirname(__file__))
-files.remove("__init__.py")
-__all__ = [f[:-3] for f in files if f.endswith(".py")]
+__all__ = [
+    'RemoteDataCollection'
+]
