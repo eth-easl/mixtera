c850b97d53de6468ea43be749d82e0eceefb1a41
Maximilian Böther <maxi+git@boether.de>
2024-02-20 12:45:11 +0100

    cleanup after cherry pcik

--- mixtera/utils/__init__.py
+++ mixtera/utils/__init__.py
@@ -2,8 +2,6 @@
 This submodule contains general utility functions
 """
 
-import os
-
 from .utils import defaultdict_to_dict, flatten, merge_defaultdicts, ranges  # noqa: F401
 
 __all__ = ["defaultdict_to_dict", "flatten", "merge_defaultdicts", "ranges"]
