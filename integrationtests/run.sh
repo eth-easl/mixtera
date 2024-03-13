1f76b6f4fdcfdadd7b2316cca58a1c2b02ec7726
Maximilian Böther <maxi+git@boether.de>
2024-03-13 10:42:51 +0100

    try to run integrationtests on githbu

--- integrationtests/run.sh
+++ integrationtests/run.sh
@@ -5,8 +5,19 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 echo "Integration tests are located in $SCRIPT_DIR"
 echo "Running as user $USER"
 
+echo "Checking whether Mixtera is available in current environment."
+MIXTERAPATH="$(python -c 'import mixtera; print(mixtera.__path__[0])')"
+if [ $? -eq 0 ]; then
+  echo "Mixtera is available at $MIXTERAPATH"
+else
+  echo "Mixtera is not available. Please run this in an environment with Mixtera installed."
+  exit 1
+fi
+
 echo "Running local data collection tests"
 python $SCRIPT_DIR/local_data_collection/test_local_collection.py
+
+echo "TODO: Start Mixtera Server here for tests" # TODO(MaxiBoether): do this.
 
 echo "Running remote data collection tests"
 python $SCRIPT_DIR/remote_data_collection/test_remote_collection.py
