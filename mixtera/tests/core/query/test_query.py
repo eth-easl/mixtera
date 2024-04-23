53008233a109af1bf3e838c897c674cfebbbaf8a
DanGraur <dan.graur@inf.ethz.ch>
2024-04-23 12:38:13 +0200

    Added some testing utilities for the chunking logic.

--- mixtera/tests/core/query/test_query.py
+++ mixtera/tests/core/query/test_query.py
@@ -21,7 +21,75 @@ class MockOperator(Operator):
         self.results.append_entry("field", "value", "did", "fid", (0, 2))
 
 
+class ComplexMockOperator(Operator):
+    def __init__(self, name, len_results: int = 1):
+        super().__init__()
+        self.name = name
+        self.len_results = len_results
+
+    def display(self, level):
+        print("-" * level + self.name)
+
+    def execute(self, mdc):
+        del mdc
+        self.results = IndexFactory.create_index(IndexTypes.IN_MEMORY_DICT_RANGE)
+        self.results._index = {
+            "language": {
+                "french": {
+                    0: {
+                        0: [(0, 50), (100, 150), (200, 300)],
+                        1: [(100, 350)],
+                        2: []  # This should never be the case, but good to test
+                    },
+                    1: {
+                        0: [(25, 50), (60, 100)],
+                        1: [(0, 50)]
+                    },
+                },
+                "english": {
+                    0: {
+                        0: [(25, 75), (140, 210), (300, 400)],
+                        1: [(50, 150)],
+                        2: [(10, 20)]
+                    },
+                    1: {
+                        0: [(50, 60), (90, 110), (130, 150)]
+                    },
+                    2: {
+                        0: [(0, 100), (150, 200)]
+                    }
+                }
+            },
+            "topic": {
+                "law": {
+                    0: {
+                        0: [(0, 50), (125, 180)],
+                        1: [(100, 150),  (200, 250)],
+                    },
+                    1: {
+                        0: [(0, 40), (50, 75), (200, 250)],
+                        1: [(20, 30)]
+                    },
+                    2: {
+                        0: [(80, 100)]
+                    }
+                },
+                "medicine": {
+                    0: {
+                        0: [(25, 75), (80, 100), (120, 200)],
+                        1: [(50, 150), (100, 150), (200, 210)],
+                    },
+                    1: {
+                        0: [(50, 60), (90, 110), (130, 150)],
+                        1: [(30, 100), (150, 200)]
+                    }
+                }
+            }
+        }
+
+
 Query.register(MockOperator)
+Query.register(ComplexMockOperator)
 
 
 class TestQueryPlan(unittest.TestCase):
@@ -125,3 +193,22 @@ class TestQuery(unittest.TestCase):
         res = list(query.results)
         res = [x._index for x in res]
         self.assertEqual(res, [{"field": {"value": {"did": {"fid": [(0, 2)]}}}}])
+
+    @patch("mixtera.core.datacollection.MixteraDataCollection._get_dataset_func_by_id")
+    @patch("mixtera.core.datacollection.MixteraDataCollection._get_dataset_type_by_id")
+    @patch("mixtera.core.datacollection.MixteraDataCollection._get_file_path_by_id")
+    def test_chunker_index(
+        self,
+        mock_get_file_path_by_id: MagicMock,
+        mock_get_dataset_type_by_id: MagicMock,
+        mock_get_dataset_func_by_id: MagicMock,
+    ):
+        mock_get_file_path_by_id.return_value = "test_file_path"
+        mock_get_dataset_type_by_id.return_value = "test_dataset_type"
+        mock_get_dataset_func_by_id.return_value = lambda x: x
+
+        query = Query.for_job("job_id").complexmockoperator("test")
+        assert self.client.execute_query(query, 1)
+
+        chunk_index = query.results._temp_chunker()
+        print(chunk_index)
