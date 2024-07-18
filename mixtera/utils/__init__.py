e128459de64881e7ca49addb037173ca1b07e0ef
Maximilian Böther <maxi+git@boether.de>
2024-07-18 13:21:22 +0200

    Updates with a very high timeout to see if it still occurs

--- mixtera/utils/__init__.py
+++ mixtera/utils/__init__.py
@@ -5,10 +5,15 @@ This submodule contains general utility functions
 from .utils import (  # noqa: F401
     defaultdict_to_dict,
     flatten,
+    hash_string,
+    list_shared_memory,
+    max_shm_len,
     merge_dicts,
     numpy_to_native_type,
     ranges,
+    remove_shm_from_resource_tracker,
     run_async_until_complete,
+    shm_usage,
     wait_for_key_in_dict,
 )
 
@@ -20,4 +25,9 @@ __all__ = [
     "numpy_to_native_type",
     "run_async_until_complete",
     "wait_for_key_in_dict",
+    "remove_shm_from_resource_tracker",
+    "shm_usage",
+    "list_shared_memory",
+    "hash_string",
+    "max_shm_len",
 ]
