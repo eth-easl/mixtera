1a11c6e779b097c21040f61160d7f690af9751e1
Xiaozhe Yao <askxzyao@gmail.com>
2024-07-24 15:05:28 +0200

    manual mix code

---
+++ experiments/datasets/select_mix_ni.py
@@ -0,0 +1,56 @@
+
+import os
+import json
+import numpy as np
+
+def select(args):
+    print(args)
+    with open(args.train_jsonl, "r") as f:
+        data = [json.loads(line) for line in f]
+    dataset_size = len(data)
+    desired_size = int(dataset_size * args.portion)
+    categories = set([x['meta']['categories'][0] for x in data])
+    count = {x: len([y for y in data if y['meta']['categories'][0] == x]) for x in categories}
+    print(f"desired_size: {desired_size}; count: {count}")
+    # a human-designed list of categories to select
+    category_selection = {
+        "Question Understanding": 59954,
+        "Word Relation Classification": 8872,
+        "Commonsense Classification": 0.5,
+        "Text Categorization": 0.2,
+        "Text Completion": 0.3,
+        "Coherence Classification": 0.1,
+    }
+    final_datasets = []
+    # prepare integer counts first
+    int_categories = [x for x in category_selection if isinstance(category_selection[x], int)]
+    float_categories = [x for x in category_selection if isinstance(category_selection[x], float)]
+    total_ratio_floats = sum([category_selection[x] for x in float_categories])
+    for category in int_categories:
+        subset = [x for x in data if x['meta']['categories'][0] == category]
+        subset = np.random.choice(subset, category_selection[category], replace=False).tolist()
+        final_datasets.extend(subset)
+    # remaining samples:
+    remaining_sample_size = desired_size - len(final_datasets)
+    print(f"remaining_sample_size: {remaining_sample_size}")
+    for category in float_categories:
+        subset = [x for x in data if x['meta']['categories'][0] == category]
+        subset_size = int(category_selection[category] / total_ratio_floats * remaining_sample_size)
+        try:
+            subset = np.random.choice(subset, subset_size, replace=False).tolist()
+            final_datasets.extend(subset)
+        except ValueError:
+            print(f"category: {category}; desired subset_size: {subset_size}; len(subset): {len(subset)}")
+            
+    print(f"final_datasets: {len(final_datasets)}")
+    with open(args.output_jsonl, "w") as f:
+        for conv in final_datasets:
+            f.write(json.dumps(conv) + "\n")
+    
+if __name__=="__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--train-jsonl", type=str, default=".cache/datasets/prepared/ni_train.jsonl")
+    parser.add_argument("--portion", type=float, default=0.1)
+    parser.add_argument("--output-jsonl", type=str, default=".cache/datasets/prepared/ni_train_selected.jsonl")
+    select(parser.parse_args())
