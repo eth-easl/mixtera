c17b0aa14b0720fa52f89550fc27c096c4e259e5
besteguney <63497613+besteguney@users.noreply.github.com>
2024-10-28 17:42:04 +0100

    Made some changes.

--- mixtera/core/query/mixture.py
+++ mixtera/core/query/mixture.py
@@ -307,17 +307,13 @@ class HierarchicalStaticMixture(Mixture):
             if component.submixture is not None:
                 result = self.convert_to_mixture_key_format(component.submixture)
                 for key, value in result.items():
-                    if (
-                        isinstance(component.value, List[str])
-                        or isinstance(component.value, List[int])
-                        or isinstance(component.value, List[float])
-                    ):
+                    if type(component.value) is List[str | int | float]:
                         key.add_property(mixture.property_name, component.value)
                     else:
                         key.add_property(mixture.property_name, [component.value])
                     mixture_keys[key] = value * component.weight
             else:
-                if isinstance(component.value, List):
+                if type(component.value) is List[str | int | float]:
                     mixture_keys[MixtureKey({mixture.property_name: component.value})] = component.weight
                 else:
                     mixture_keys[MixtureKey({mixture.property_name: [component.value]})] = component.weight
