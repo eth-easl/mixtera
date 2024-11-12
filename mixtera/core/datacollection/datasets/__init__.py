cf377b2021d0b1a3dcf457540f3d4d27217ea1bf
Maximilian Böther <2116466+MaxiBoether@users.noreply.github.com>
2024-11-12 16:41:30 +0100

    feat: Parquet dataset support (#138)
    Datasets such as fineweb come in parquet format. With this PR, we
    support parquet datasets and have an example parser for fineweb.

--- mixtera/core/datacollection/datasets/__init__.py
+++ mixtera/core/datacollection/datasets/__init__.py
@@ -5,5 +5,6 @@ This submodule contains implementations for different datasets
 from .dataset import Dataset  # noqa: F401
 from .dataset_type import DatasetType  # noqa: F401
 from .jsonl_dataset import JSONLDataset  # noqa: F401
+from .parquet_dataset import ParquetDataset  # noqa: F401
 
-__all__ = ["Dataset", "DatasetType", "JSONLDataset"]
+__all__ = ["Dataset", "DatasetType", "JSONLDataset", "ParquetDataset"]
