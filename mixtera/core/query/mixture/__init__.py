2d06ad46478e32149c9b4dda77461773fd15e1d1
besteguney <63497613+besteguney@users.noreply.github.com>
2024-11-15 22:34:57 +0100

    Updated the init file.

--- mixtera/core/query/mixture/__init__.py
+++ mixtera/core/query/mixture/__init__.py
@@ -1,9 +1,13 @@
 from .arbitrary_mixture import ArbitraryMixture
 from .hierarchical_static_mixture import Component, HierarchicalStaticMixture, MixtureNode
 from .inferring_mixture import InferringMixture
+from .mixture import Mixture
+from .mixture_key import MixtureKey
 from .static_mixture import StaticMixture
 
 __all__ = [
+    "Mixture",
+    "MixtureKey",
     "ArbitraryMixture",
     "StaticMixture",
     "InferringMixture",
