ff6316e929825163b7a627d868e9ca17fcb81954
Tolga Kerimoğlu <tolgakerimoglu@outlook.com>
2025-01-24 16:04:59 +0100

    remove print

--- mixtera/core/datacollection/index/parser/parser_collection.py
+++ mixtera/core/datacollection/index/parser/parser_collection.py
@@ -186,7 +186,6 @@ class GenericMetadataParser(MetadataParser):
 
     def parse(self, line_number: int, payload: Any, **kwargs: Optional[dict[Any, Any]]) -> None:
         dataset_name = kwargs.get("dataset_name")
-        print(f"dataset_name: {dataset_name}")
         self.add_metadata(sample_id=line_number, dataset=dataset_name)
 
 
